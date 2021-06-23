c9f9a2f8224122fda483fe3475023fb386b43b8e
marysukiasyan <marysukiasyan@gmail.com>
2021-06-23 21:03:50 +0400

    adding task1 first method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import task1.Point;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //task1 distance() method testing
+        Point point1 = new Point(-4, 3);
+        System.out.println(point1.distance());
     }
 }
---
+++ src/task1/Point.java
@@ -0,0 +1,39 @@
+package task1;
+
+import static java.lang.Math.abs;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(){
+        double firstLeg = abs(getX());
+        double secondLeg = abs(getY());
+        double hypotenuseLength = Math.sqrt(Math.pow(firstLeg, 2) + Math.pow(secondLeg, 2
+        ));
+        return hypotenuseLength;
+
+    } 
+
+}
